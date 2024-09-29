92d9134c171c10c54d0b8ec6d8e640e59cf2b200
Snake Sanders <elmarian22@gmail.com>
2024-09-29 20:24:20 +0200

    add example of const int * const

---
+++ pointers/p4/main.cpp
@@ -0,0 +1,16 @@
+#import <cstdlib>
+#import <iostream>
+using namespace std;
+
+int main(void) {
+  int month = 11;
+
+  const int *pMonth_a = &month;
+  // Error: month value can't be changed, it is const int
+  //*pMonth_a = 12;
+
+  const int *const pMonth_b = &month;
+  const int holliday = 6;
+  // pMonth_b = &holliday; // Error: the pointer cannot be reassigned
+  return 0;
+}
